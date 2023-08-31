691ce97205294db385bed11a72d6758977b8fe4e
Bismay <bismaypurkayastha5467@gmail.com>
2023-09-01 01:16:04 +0530

    global constant removed to constants.mjs file.

--- server/server.js
+++ server/server.js
@@ -4,16 +4,15 @@ import express from 'express';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 
+import { DEFAULT_DEV_PORT, EXIT_FAILURE } from './common/constants.mjs';
+
 import { ERROR_CODES } from './common/statusCode.mjs';
 import { ErrorHandler, globalErrorHandler } from './utils/errorHandler.mjs';
 
 dotenv.config();
 const app = express();
 
-const DEFAULT_DEV_PORT = 3000;
 const PORT = Number(process.env.DEV_PORT) || DEFAULT_DEV_PORT;
-
-const EXIT_FAILURE = 1;
 
 process.on('uncaughtException', (error) => {
   console.error('UNCAUGHT EXCEPTION! Shutting down...');
