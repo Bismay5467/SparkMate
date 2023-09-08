e381b84c32545a3ac486fce69eb1d70e1fe93dda
bismay <bismaypurkayastha5467@gmail.com>
2023-09-09 00:57:25 +0530

    inbox mock data added to test inbox api endpoints.

---
+++ server/mocks/chats/inbox.mocks.mjs
@@ -0,0 +1,17 @@
+export const inbox1 = {
+  userID: ['64faff276e5091939c4eee49', '64fb1dff50118fc46db59d09'],
+};
+export const inbox2 = {
+  userID: ['64faff276e5091939c4eee49', '64fb1e4744f37fc983752a44'],
+};
+export const inbox3 = {
+  userID: ['64fb1dff50118fc46db59d09', '64fb1e4744f37fc983752a44'],
+};
+export const inbox4 = {
+  userID: ['64fb1dff50118fc46db59d09', '64fb1dff50118fc46db59d09'],
+};
+/*
+rahul : 64faff276e5091939c4eee49
+arkojeet : 64fb1dff50118fc46db59d09
+biplaw : 64fb1e4744f37fc983752a44
+*/
