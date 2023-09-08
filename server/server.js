bba3a6d4d1cbb2f72e170313b81a7c59d232599f
bismay <bismaypurkayastha5467@gmail.com>
2023-09-09 04:15:45 +0530

    changes made.

--- server/server.js
+++ server/server.js
@@ -6,8 +6,7 @@ import cookieParser from 'cookie-parser';
 
 import { DEFAULT_DEV_PORT, EXIT_FAILURE } from './common/constants.mjs';
 
-// eslint-disable-next-line import/no-named-default
-import { default as ChatRoutes } from './routes/chats/routes.mjs';
+import { ChatRouter } from './routes/index.mjs';
 import { ERROR_CODES } from './common/statusCode.mjs';
 import { ErrorHandler, globalErrorHandler } from './utils/errorHandler.mjs';
 
@@ -27,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compression({ level: 6, threshold: 1000 }));
 
-app.use('/chats', ChatRoutes);
+app.use('/api/chats', ChatRouter);
 
 app.all('*', (req, _res, next) => {
   next(
