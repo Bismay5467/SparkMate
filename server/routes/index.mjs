54879cfe22cc04f757557dc4f92903e7506f3d1d
bismay <bismaypurkayastha5467@gmail.com>
2023-09-09 04:43:47 +0530

    minor bug fixed relating to import statement.

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -1,3 +1,4 @@
-import * as ChatRouter from './chats/routes.mjs';
+// eslint-disable-next-line import/no-named-default
+import { default as ChatRouter } from './chats/routes.mjs';
 
 export { ChatRouter };
