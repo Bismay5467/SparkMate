b044c9fe8244e11d1c0d9703c90e68d103f4b3d8
bismay <bismaypurkayastha5467@gmail.com>
2023-09-10 15:50:41 +0530

    controllers added to seed data for inbox and chats collections in mongodb.

---
+++ server/controllers/chats/__tests__/inboxData.mjs
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+import { InboxCollection } from '../../../model/index.mjs';
+import asyncHandler from '../../../utils/asyncHandler.mjs';
+import connectDB from '../../../config/database.mjs';
+import { inbox1, inbox2, inbox3 } from '../../../mocks/chats/inbox.mocks.mjs';
+
+const SUCCESSS_STATUS_CODE = 200;
+
+const insertInboxData = asyncHandler(async (req, res) => {
+  await connectDB();
+
+  const response = await InboxCollection.insertMany([inbox1, inbox2, inbox3]);
+  // eslint-disable-next-line no-console
+  console.log(response);
+  await mongoose.disconnect();
+
+  res
+    .status(SUCCESSS_STATUS_CODE)
+    .json({ message: 'data inserted successfully.', success: true });
+});
+
+export default insertInboxData;
---
+++ server/controllers/chats/__tests__/messageData.mjs
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+import { MessageCollection } from '../../../model/index.mjs';
+import asyncHandler from '../../../utils/asyncHandler.mjs';
+import connectDB from '../../../config/database.mjs';
+import {
+  message1,
+  message2,
+  message3,
+} from '../../../mocks/chats/message.mocks.mjs';
+
+const SUCCESSS_STATUS_CODE = 200;
+
+const insertMessageData = asyncHandler(async (req, res) => {
+  await connectDB();
+
+  const response = await MessageCollection.insertMany([
+    message1,
+    message2,
+    message3,
+  ]);
+  // eslint-disable-next-line no-console
+  console.log(response);
+  await mongoose.disconnect();
+
+  res
+    .status(SUCCESSS_STATUS_CODE)
+    .json({ message: 'data inserted successfully.', success: true });
+});
+
+export default insertMessageData;
