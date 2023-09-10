4703c4aaa08e504fa2d76ba14c99b6f35bc16f67
bismay <bismaypurkayastha5467@gmail.com>
2023-09-10 15:52:52 +0530

    controllers added to index file.

--- server/controllers/index.mjs
+++ server/controllers/index.mjs
@@ -3,5 +3,20 @@ import deleteChats from './chats/deleteChats.mjs';
 import fetchAllChats from './chats/fetchAllChats.mjs';
 import fetchChats from './chats/fetchChats.mjs';
 
+// eslint-disable-next-line import/no-named-default
+import { default as SeedInboxData } from './chats/__tests__/inboxData.mjs';
+// eslint-disable-next-line import/no-named-default
+import { default as SeedMessageData } from './chats/__tests__/messageData.mjs';
+// eslint-disable-next-line import/no-named-default
+import { default as SeedUserData } from './users/__tests__/userData.mjs';
+
 // eslint-disable-next-line object-curly-newline
-export { fetchAllChats, deleteChats, fetchChats, chatSetUp };
+export {
+  fetchAllChats,
+  deleteChats,
+  fetchChats,
+  chatSetUp,
+  SeedInboxData,
+  SeedMessageData,
+  SeedUserData,
+};
