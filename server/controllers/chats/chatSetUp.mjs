135dfc44481421e74a7b13877334695b6bbb7f15
bismay <bismaypurkayastha5467@gmail.com>
2023-09-12 18:22:57 +0530

    service logic for chat setup moved to service directory.

--- server/controllers/chats/chatSetUp.mjs
+++ server/controllers/chats/chatSetUp.mjs
@@ -1,40 +1,15 @@
-import mongoose from 'mongoose';
-
-import { InboxCollection } from '../../model/index.mjs';
-import { MAX_QUERY_EXEC_TIME_MS } from '../../common/constants.mjs';
+import { ChatSetUp } from '../../services/index.mjs';
 import { SUCESS_CODES } from '../../common/statusCode.mjs';
 import asyncHandler from '../../utils/asyncHandler.mjs';
-import connectDB from '../../config/database.mjs';
 
 const chatSetUp = asyncHandler(async (req, res) => {
   const { userID } = req.body;
 
   // const userID = ['64fb8867d6497a8738475cab', '64fb8867d6497a8738475caa'];
 
-  const query = {
-    userID: {
-      $all: [
-        { $elemMatch: { $eq: new mongoose.Types.ObjectId(userID[0]) } },
-        { $elemMatch: { $eq: new mongoose.Types.ObjectId(userID[1]) } },
-      ],
-    },
-  };
-  const update = { userID };
-  const option = { upsert: true, new: true };
-
-  const projection = { _id: 1 };
-
-  await connectDB();
-
-  const response = await InboxCollection.findOneAndUpdate(query, update, option)
-    .select(projection)
-    .maxTimeMS(MAX_QUERY_EXEC_TIME_MS)
-    .lean()
-    .exec();
+  const response = await ChatSetUp(userID);
 
   const { _id: inboxID } = response;
-
-  await mongoose.disconnect();
 
   return res.status(SUCESS_CODES.CREATED).json({
     message:
--- server/services/index.mjs
+++ server/services/index.mjs
@@ -1,4 +1,6 @@
 // eslint-disable-next-line import/no-named-default
+import { default as ChatSetUp } from './chats/chatSetUp.service.mjs';
+// eslint-disable-next-line import/no-named-default
 import { default as DeleteChats } from './chats/deleteChats.service.mjs';
 // eslint-disable-next-line import/no-named-default
 import { default as GetAllChats } from './chats/getAllChats.service.mjs';
@@ -7,4 +9,5 @@ import { default as GetChats } from './chats/getChats.service.mjs';
 // eslint-disable-next-line import/no-named-default
 import { default as InsertChats } from './chats/insertChats.service.mjs';
 
-export { InsertChats, DeleteChats, GetChats, GetAllChats };
+// eslint-disable-next-line object-curly-newline
+export { InsertChats, DeleteChats, GetChats, GetAllChats, ChatSetUp };
