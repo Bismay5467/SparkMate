d1f7420c37406bb3a34549c84fe2d0e0fe1e6135
BiplawSingh <biplawgaurav@gmail.com>
2023-09-16 16:53:19 +0530

    Modified user configs.

--- server/config/default.config.mjs
+++ server/config/default.config.mjs
@@ -3,7 +3,7 @@ export const DEFAULT_DB_CONFIG = Object.freeze({
   MONGO_TIMEOUT_MS: 5000,
   MONGO_USER: 'austin_3_16',
   MONGO_PASSWORD: 'austin696969',
-  MONGO_DB: 'users',
+  MONGO_DB: 'SparkMate',
 });
 
 export const MONGO_URI = `mongodb+srv://${DEFAULT_DB_CONFIG.MONGO_USER}:${DEFAULT_DB_CONFIG.MONGO_PASSWORD}@cluster0.fs9uxg6.mongodb.net/${DEFAULT_DB_CONFIG.MONGO_DB}?retryWrites=true&w=majority`;
