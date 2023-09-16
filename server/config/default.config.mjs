fb7bc2d17dea7345a9fc5970cb2f36f9fc217cb0
BiplawSingh <biplawgaurav@gmail.com>
2023-09-16 11:41:09 +0530

    Resolved linting issues

--- server/config/default.config.mjs
+++ server/config/default.config.mjs
@@ -1,9 +1,9 @@
 export const DEFAULT_DB_CONFIG = Object.freeze({
   MONGO_MAX_POOLSIZE: 100,
   MONGO_TIMEOUT_MS: 5000,
-  MONGO_USER: 'Bismay',
-  MONGO_PASSWORD: 'Bismay5467',
-  MONGO_DB: 'dating_app',
+  MONGO_USER: 'austin_3_16',
+  MONGO_PASSWORD: 'austin696969',
+  MONGO_DB: 'users',
 });
 
-export const MONGO_URI = `mongodb+srv://${DEFAULT_DB_CONFIG.MONGO_USER}:${DEFAULT_DB_CONFIG.MONGO_PASSWORD}@cluster0.usbeazf.mongodb.net/${DEFAULT_DB_CONFIG.MONGO_DB}?retryWrites=true&w=majority`;
+export const MONGO_URI = `mongodb+srv://${DEFAULT_DB_CONFIG.MONGO_USER}:${DEFAULT_DB_CONFIG.MONGO_PASSWORD}@cluster0.fs9uxg6.mongodb.net/${DEFAULT_DB_CONFIG.MONGO_DB}?retryWrites=true&w=majority`;
